e7340251b4c2d492ac92aa061b5b7a18cb320013
William Castro <williamweb2.0@gmail.com>
2023-09-12 13:40:47 -0300

    Update ExpandSubdomains.sh

--- ExpandSubdomains.sh
+++ ExpandSubdomains.sh
@@ -17,14 +17,11 @@ findomain -t $1 | egrep -v "A error|Searching|Target|Job finished|Good luck" | a
 
 #after yout need send to nuclei use ssl names template
 nuclei -l $1_subdomains.txt -t ssl/ssl-dns-names.yaml -o $1_subdomains_ssl.txt 
-#rm $1_subdomains.txt
 
 #this step i just make regular expression to clear results and save using anew
 cat $1_subdomains_ssl.txt | cut -d "[" -f5 | cut -d "]" -f1 | tr ',' '\n' | anew $1_subdomains_ssl_new.txt 
 cat $1_subdomains_ssl_new.txt | grep -v -E "microsoft.com|cloudflare.com|big-ip.com|f5.com|teams.com|office365.com|microsoft365.com|microsoftonline-p.com|microsoftonline.com|live.com|live.net|office.net|trafficmanager.net|officeppe.net|cloudflare-dns.com|hotmail.com|office.com|microsoft|meet.lync.com|o365.com|outlook.com|officeppe.net|cloudfront|cloudflaressl.com" | anew $1_subdomains_ssl_cleaned.txt
-#rm $1_subdomains_ssl_new.txt
 #TODO: list of waf's to remove from results
-#rm $1_subdomains_ssl.txt
 
 
 ####
@@ -32,19 +29,16 @@ cat $1_subdomains_ssl_new.txt | grep -v -E "microsoft.com|cloudflare.com|big-ip.
 
 #make again recon of recon
 cat $1_subdomains_ssl_cleaned.txt | cut -d "." -f2,3,4 | sort | uniq | grep "\." | anew $1_subdomains_ssl_news_domains.txt
-#rm $1_subdomains_ssl_cleaned.txt
 #
 nuclei -l $1_subdomains_ssl_news_domains.txt -t ssl/ssl-dns-names.yaml -o $1_subdomains_2recon.txt
 #
 cat $1_subdomains_ssl_news_domains.txt | anew $1_subdomains_2recon.txt
-#rm $1_subdomains_ssl_news_domains.txt
 #
 #this step i just make regular expression to clear results and save using anew
 cat $1_subdomains_ssl.txt | cut -d "[" -f5 | cut -d "]" -f1 | tr ',' '\n' | anew $1_subdomains_ssl_new.txt 
 cat $1_subdomains_ssl_new.txt | grep -v -E "microsoft.com|cloudflare.com|big-ip.com|f5.com|teams.com|office365.com|microsoft365.com|microsoftonline-p.com|microsoftonline.com|live.com|live.net|office.net|trafficmanager.net|officeppe.net|cloudflare-dns.com|hotmail.com|office.com|microsoft|meet.lync.com|o365.com|outlook.com|officeppe.net|cloudfront|cloudflaressl.com" | anew $1_subdomains_ssl_cleaned_2recon_2cleaned.txt
-#rm $1_subdomains_ssl_new.txt
 #TODO: list of waf's to remove from results
-#rm $1_subdomains_2recon.txt
+
 #here i just show quantity
 cat $1_subdomains_ssl_cleaned_2recon_2cleaned.txt | wc -l
 
@@ -54,7 +48,6 @@ cat $1_subdomains_ssl_cleaned_2recon_2cleaned.txt | wc -l
 
 #Now i check who is online and active using tool httpx
 cat $1_subdomains_ssl_cleaned_2recon_2cleaned.txt | httpx -silent | anew $1_subdomains.txt 
-#rm $1_subdomains_ssl_cleaned_2recon_2cleaned.txt
 
 #Here i show results and quantity
 cat $1_subdomains.txt
@@ -62,7 +55,6 @@ cat $1_subdomains.txt
 echo ""
 echo "Subtotal of subdomains founds before clean:"
 cat $1_subdomains_ssl_cleaned.txt | wc -l
-#rm $1_subdomains_ssl_cleaned.txt
 echo ""
 echo "Total subdomains founds:"
 cat $1_subdomains.txt | wc -l
@@ -71,6 +63,15 @@ echo "Result save into the file:"
 echo $1_subdomains.txt
 echo ""
 echo "keep hacking, by @OPenTester"
+
+#clear old files
+rm $1_subdomains_ssl.txt
+rm $1_subdomains_ssl_new.txt
+rm $1_subdomains_ssl_cleaned.txt
+rm $1_subdomains_ssl_news_domains.txt
+rm $1_subdomains_ssl_cleaned_2recon_2cleaned.txt
+
+
 
 #no more...
 #keep hacking, by @OPenTester
